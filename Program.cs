18a1de41882ff4cbba99d4c2161c638721277d9d
George Henrique Wurthmann <gemakai@gmail.com>
2021-01-02 18:51:05 -0300

    Adicionado comando para imprimir a data de hoje.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ControleDeVersaoComGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.ToLongDateString());
         }
     }
 }
