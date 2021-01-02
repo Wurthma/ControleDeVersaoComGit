1dc7a190d1510b934f9effcabb943647ee80e936
George Henrique Wurthmann <gemakai@gmail.com>
2021-01-02 20:27:58 -0300

    Adicionado primeiro exercício

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace ControleDeVersaoComGit
+namespace HackerHankExercises
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(DateTime.Now.ToLongDateString());
-            Console.WriteLine("Desenvolvedor: George Henrique Wurthmann");
+            // fazer chamadas dos métodos abaixo para testes...
+        }
+
+        // https://www.hackerrank.com/challenges/simple-array-sum/problem
+        public static int DiagonalDifference(List<List<int>> arr)
+        {
+            int diagonal1Sum = 0;
+            int diagonal2Sum = 0;
+            for(int i = 0, j = arr.Count()-1; i < arr.Count(); i++, j--)
+            {
+                diagonal1Sum += arr[i][i];
+                diagonal2Sum += arr[i][j];
+            }
+            return Math.Abs(diagonal1Sum - diagonal2Sum);
         }
     }
 }
